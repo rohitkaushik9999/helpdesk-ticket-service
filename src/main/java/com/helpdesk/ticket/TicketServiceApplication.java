8f52c58e3d22a9e8898348fb76bfc52b60bcf231
rohit.kaushik <rohit.kaushik@LP-5CD742BS0C.HCLT.CORP.HCL.IN>
2019-10-18 18:26:00 +0530

    first commit

---
+++ src/main/java/com/helpdesk/ticket/TicketServiceApplication.java
@@ -0,0 +1,17 @@
+package com.helpdesk.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaAuditing
+@EnableJpaRepositories
+@SpringBootApplication
+public class TicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/helpdesk/ticket/controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.helpdesk.ticket.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helpdesk.ticket.dto.User;
+
+@RestController
+public class LoginController {
+
+	
+	 @RequestMapping(path = "/check-login", method = RequestMethod.POST) 
+	 public ResponseEntity<Object> checkLogin(@RequestBody User user) {
+			
+		 if(user != null) {
+			 if(user.getUsername().equals("admin") && user.getPassword().equals("admin")) {
+				 user.setToken("qLGJXpqJiN-17MrH9eXjeJqRPBVdvLwumkfj54iRp9HHPkOFPfBBRz84Bcw0UNL6yJy.xD24HQj3Gr-XfC42q4k8YLreehZIJn-ed5DhS9auGjvGGMYNA6ePdxXl59ak");
+				 return new ResponseEntity<>(user, HttpStatus.OK); 
+			 }	
+		 }
+		 
+		 return new ResponseEntity<>("Failure", HttpStatus.UNPROCESSABLE_ENTITY);
+		 
+	 }	 
+	 
+	 
+	 @RequestMapping(path = "/testing", method = RequestMethod.GET) 
+	 public ResponseEntity<String> testing() {
+			
+		 return new ResponseEntity<String>("Success", HttpStatus.OK);
+		 
+	 }	 
+}
