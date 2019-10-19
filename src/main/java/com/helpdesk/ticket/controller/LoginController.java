f33ed12df2324e95df082205bf03725915313856
rohit.kaushik <rohit.kaushik@LP-5CD742BS0C.HCLT.CORP.HCL.IN>
2019-10-19 15:37:49 +0530

    cors filter

--- src/main/java/com/helpdesk/ticket/controller/LoginController.java
+++ src/main/java/com/helpdesk/ticket/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.helpdesk.ticket.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.helpdesk.ticket.dto.User;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
 	
